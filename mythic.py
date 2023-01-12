3108853ce4dcce6cef6a1d7207f18aacbd9bbb9b
Thomas HERBIN <the@apik.cloud>
2023-01-12 19:33:15 +0100

    [IMP] add command to return last week datas

--- mythic.py
+++ mythic.py
@@ -27,7 +27,7 @@ def get_roster():
             character.pop(key)
     return roster
 
-def get_mythics_done():
+def get_mythics_done(week):
     roster = get_roster()
     headers = { 
         "accept": "application/json", 
@@ -35,16 +35,18 @@ def get_mythics_done():
 
     roster_mythics_done = {}
 
+    week_url = 'mythic_plus_previous_weekly_highest_level_runs' if week == 'last' else 'mythic_plus_weekly_highest_level_runs'
+
     for number, character in enumerate(roster):
         name = character['name']
         realm = character['realm'].replace('Marecage de Zangar', 'Marécage de Zangar').replace(' ', '-')
-        result = requests.get('https://raider.io/api/v1/characters/profile?region=eu&realm=%s&name=%s&fields=mythic_plus_weekly_highest_level_runs' % (realm, name), headers=headers)
+        result = requests.get('https://raider.io/api/v1/characters/profile?region=eu&realm=%s&name=%s&fields=%s' % (realm, name, week_url), headers=headers)
         mythics_done = result.json()
 
         mythic_datas = []
 
         # we only want weekly mythics done
-        for mythic_done in mythics_done['mythic_plus_weekly_highest_level_runs']:
+        for mythic_done in mythics_done[week_url]:
             mythic_datas.append(mythic_done['mythic_level'])
 
         roster_mythics_done[character['name']] = mythic_datas
@@ -71,10 +73,15 @@ def format_mythics_done(roster_mythics_done, min_lvl):
     string_to_return += '\n```'
     return string_to_return
 
-@tree.command(name="mythics_done", description="Show chests based on mythics plus done", guild=discord.Object(id=356909829652217857))
-async def mythics_done(interaction, level: typing.Optional[int] = 0):
+@tree.command(name="mythics", description="Show chests based on mythics plus done", guild=discord.Object(id=356909829652217857))
+@app_commands.choices(semaine=[
+    app_commands.Choice(name="Actuelle", value="current"),
+    app_commands.Choice(name="Dernière", value="last"),
+    ])
+async def mythics(interaction, semaine: typing.Optional[app_commands.Choice[str]], niveau: typing.Optional[int] = 0):
+    semaine = 'current' if semaine == None else semaine.value
     await interaction.response.send_message('Commande lancée...')
-    response = format_mythics_done(get_mythics_done(), level)
+    response = format_mythics_done(get_mythics_done(semaine), niveau)
     await interaction.edit_original_response(content=response)
 
 @client.event
