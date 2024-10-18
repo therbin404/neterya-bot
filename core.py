b21ca246517dc114f065b2b520829a6250047714
therbin404 <thomas.g.herbin@gmail.com>
2024-10-18 23:59:05 +0200

    [REM] remove legacy code

--- core.py
+++ core.py
@@ -1,39 +1,9 @@
 import discord
-# import typing
 import os
-# from discord import app_commands
 from dotenv import load_dotenv
 import functions
 
 load_dotenv()
-
-# intents = discord.Intents.default()
-# intents.message_content = True
-# client = discord.Client(intents=intents)
-# tree = app_commands.CommandTree(client)
-# guild_obj = discord.Object(id=int(os.getenv('DISCORD_ID')))
-
-# admin_id = 222844821264531456
-
-# @tree.command(name="lineup", description="Publish lineup for next raid", guild=guild_obj)
-# async def lineup(interaction, date: typing.Optional[str] = ''):
-#     # defer is here to quick respond to discord, to avoid timeout of application
-#     await interaction.response.defer()
-
-#     admin = await client.fetch_user(admin_id)
-#     await admin.send('%s a utilisé la commande /%s (avec les arguments: %s)' %(interaction.user, interaction.command.name, date))
-
-#     Lineup = functions.lineup.Lineup()
-#     # return [(bool), errors or string]
-#     # first argument if there's any errors
-#     # second is errors, or expected response if there's no errors
-#     response = Lineup.get_lineup(date)
-    
-#     if response[0]:
-#         await interaction.delete_original_response()
-#         await interaction.user.send(content=response[1])
-#     elif not response[0]:
-#         await interaction.edit_original_response(content=response[1])
 
 
 bot = discord.Bot()
